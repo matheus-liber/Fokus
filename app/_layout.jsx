8fd23b725195e017dcaa3a3d7d4852b497209c47
matheus-liber <matheuslibergt@gmail.com>
2025-07-18 15:17:49 -0300

    Aprimorando a Drawer Navigation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,22 +4,49 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function Layout () {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer />
+            <Drawer
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#021123',
+                    },
+                    headerTintColor: '#FFF',
+                    drawerStyle: {
+                        backgroundColor: '#021123'
+                    },
+                    drawerLabelStyle: {
+                        color: '#FFF'
+                    }
+                }}
+            >
+                <Drawer.Screen 
+                    name='index'
+                    options={{
+                        headerShown: false,
+                        drawerItemStyle: { display: 'none' }
+                    }}
+                />
+                <Drawer.Screen 
+                    name='add-task/index'
+                    options={{
+                        drawerItemStyle: { display: 'none' }
+                    }}
+                />
+                <Drawer.Screen 
+                    name='pomodoro'
+                    options={{
+                        drawerLabel: 'Timer',
+                        title: ''
+                    }}
+                />
+                <Drawer.Screen 
+                    name='tasks/index'
+                    options={{
+                        drawerLabel: 'Lista de tarefas',
+                        title: ''
+                    }}
+                />
+            </Drawer>
         </GestureHandlerRootView>
     )
 }
 
-/*
-
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer;
-
-export default function Layout () {
-    return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer />
-        </GestureHandlerRootView>
-    )
-}
-
-*/
